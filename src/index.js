d322cfa5060f19e696817851d0aea1c49545f576
João Casemiro <jv.casemiro@icloud.com>
2021-08-05 21:53:06 -0300

    Subindo servidor HTTP

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Importando express
+const express = require('express');
+
+const app = express();
+
+app.use(express);
+
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
